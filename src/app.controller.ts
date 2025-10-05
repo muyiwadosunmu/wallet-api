0dfb381fab60b1c1ae7410904c1ec48cd5aecde4
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gamail.com>
2025-10-05 02:25:37 +0100

    feat: implement health check endpoint in AppService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('v1/health-check')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  healthCheck() {
+    return this.appService.healthCheck();
+  }
 }
