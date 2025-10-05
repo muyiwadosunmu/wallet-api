d6f8389816e8ac06bb69f11cdd3d6e68b2c286be
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2025-10-05 02:25:37 +0100

    feat: implement health check endpoint in AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Welcome to Cotrackr📝 !';
+  healthCheck(): string {
+    return `{
+  "status": "ok",
+  "timestamp": "${new Date().toISOString()}"
+}`;
   }
 }
